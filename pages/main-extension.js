068fcb8a377bd13c56ea5a010081b51074f67c6a
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2017-02-07 09:15:07 +0530

    GET request for html of extension(incomplete)

--- pages/main-extension.js
+++ pages/main-extension.js
@@ -2,8 +2,24 @@
 var app = angular.module("workbenchEnhancerApp",[]);
 
 app.directive("mainExtension",function(){
-  var mainExtension = {};
-  mainExtension.restrict = "A";
-  mainExtension.templateUrl = "templates/main-extension.html";
+  var request = new HttpRequest();
+  var mainExtension ={};
+  mainExtension.restrict="A";
+  request.get(chrome.extension.getURL("templates/main-extension.html"),function(response){
+    mainExtension.template = response;
+  });
   return mainExtension;
 })
+
+var HttpRequest = function(){
+  this.get = function(url,callBack){
+    var xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange = function(){
+      if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200){
+        callBack(xmlHttpRequest.responseText);
+      }
+    }
+    xmlHttpRequest.open("GET",url,true);
+    xmlHttpRequest.send(null);
+  }
+}
