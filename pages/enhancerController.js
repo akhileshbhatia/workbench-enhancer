481b3d707526081611667de171f80979599d037a
Akhilesh Bhatia <akhileshbhatia>
2017-02-08 18:46:35 +0530

    initial code of query button

--- pages/enhancerController.js
+++ pages/enhancerController.js
@@ -1,8 +1,20 @@
 app.controller("workbenchEnhancerController",function($scope){
-  console.log("In angular controller");
 
   $scope.queryBtnClick = function(event){
     event.preventDefault();
-    $scope.textAreaVal = "set through the controller";
+    var date = new Date();
+    var month = parseInt(date.getMonth(),10) + 1;
+    var todaysDate = date.getDate() + "-" + month + "-" + date.getFullYear();
+    chrome.storage.local.get(todaysDate,function(data){
+      if(angular.equals({},data)){ // //empty object means a new entry for that key will be made
+        data[todaysDate] = [$scope.textAreaVal.trim()];
+      }
+      else{
+        data[todaysDate].push($scope.textAreaVal.trim());
+      }
+      chrome.storage.local.set(data,function(){
+        console.log("data added to storage");
+      })
+    });
   }
 });
