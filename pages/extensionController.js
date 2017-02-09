8d971b5999c99a39d375abfe53c206d4613dc58c
Akhilesh Bhatia <akhileshbhatia>
2017-02-09 18:47:20 +0530

    added angular service to get data

---
+++ pages/extensionController.js
@@ -0,0 +1,45 @@
+app.controller("workbenchEnhancerController",function($scope,dataService){
+  $scope.queryBtnClick = function(event){
+    event.preventDefault();
+    var date = new Date();
+    var month = parseInt(date.getMonth(),10) + 1;
+    var todaysDate = date.getDate() + "-" + month + "-" + date.getFullYear();
+    chrome.storage.local.get(todaysDate,function(data){
+      if(angular.equals({},data)){ // //empty object means a new entry for that key will be made
+        data[todaysDate] = [$scope.textAreaVal.trim()];
+      }
+      else{
+        data[todaysDate].unshift($scope.textAreaVal.trim());
+      }
+      chrome.storage.local.set(data,function(){
+        console.log("data added to storage");
+      })
+    });
+  }
+
+  $scope.getData = function(){
+    var askForPromise = dataService.GetData();
+    askForPromise.then(
+      function(data){
+        $scope.allData = data;
+      },
+      function(){
+        console.log("Some error in receiving data");
+      }
+    )
+  }
+
+  $scope.getData();
+});
+
+app.service("dataService",function($q){
+  return{
+    GetData: function(){
+      var defferdObj = $q.defer();
+      chrome.storage.local.get(null,function(data){
+        defferdObj.resolve(data);
+      })
+      return defferdObj.promise;
+    }
+  }
+})
