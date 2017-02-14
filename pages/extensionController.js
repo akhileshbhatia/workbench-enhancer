4e7ceec1b6fcac40835134d75577a4139b385163
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2017-02-14 06:08:20 +0530

    provision for adding custom dates via code

--- pages/extensionController.js
+++ pages/extensionController.js
@@ -1,10 +1,9 @@
 app.controller("workbenchEnhancerController",function($scope,$filter,dataService){
   $scope.addDataToStorage = function(event){
     event.preventDefault();
-    var date = new Date();
-    var month = parseInt(date.getMonth(),10) + 1;
-    var todaysDate = $filter("date")(new Date(),"dd MMM yyyy");
-    //var todaysDate = "9-2-2017";
+    var date = new Date(); // Use for todays date
+    //var date = new Date(2017,01,13); //new Date(yyyy,mm,dd). Use for specific dates. Months ordered from 0 in javascript
+    var todaysDate = $filter("date")(date,"dd MMM yyyy");
     chrome.storage.local.get(todaysDate,function(data){
       if(angular.equals({},data)){ // //empty object means a new entry for that key will be made
         data[todaysDate] = [$scope.textAreaVal.trim()];
@@ -30,7 +29,7 @@ app.controller("workbenchEnhancerController",function($scope,$filter,dataService
     )
   }
 
-  $scope.getData();
+  $scope.getData(); //call on page load
 
   $scope.setQueryText = function(event){
     $scope.textAreaVal = event.target.textContent.trim();
