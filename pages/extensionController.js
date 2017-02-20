53e17a25f4a0f5eb4f5a8af2f3c535256b1d394c
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2017-02-20 07:09:10 +0530

    fixed date sorting issue

--- pages/extensionController.js
+++ pages/extensionController.js
@@ -4,11 +4,11 @@ app.controller("workbenchEnhancerController",function($scope,$filter,dataService
     isFirstDisabled: false
   };
   $scope.accordionArray = [];
-  
+
   $scope.addDataToStorage = function(event){
     event.preventDefault();
     var date = new Date(); // Use for todays date
-    //var date = new Date(2017,01,13); //new Date(yyyy,mm,dd). Use for specific dates. Months ordered from 0 in javascript
+    //var date = new Date(2017,03,08); //new Date(yyyy,mm,dd). Use for specific dates. Months ordered from 0 in javascript
     var todaysDate = $filter("date")(date,"dd MMM yyyy");
     chrome.storage.local.get(todaysDate,function(data){
       if(angular.equals({},data)){ // //empty object means a new entry for that key will be made
@@ -18,48 +18,31 @@ app.controller("workbenchEnhancerController",function($scope,$filter,dataService
         data[todaysDate].unshift($scope.textAreaVal.trim());
       }
       chrome.storage.local.set(data,function(){
-        $scope.getData(); //call to getData function again to refresh view
+        getData(); //call to getData function again to refresh view
       })
     });
   }
 
-  $scope.getData = function(){
+  var getData = function(){
     var askForPromise = dataService.GetData();
     askForPromise.then(
       function(data){
-        $scope.allData = data;
+        $scope.storageData = data;
+        $scope.sortedDates = Object.keys($scope.storageData).sort(function(a,b){
+          return (new Date(b) - new Date(a));
+        });
       },
       function(){
         console.log("Some error in receiving data");
       }
     )
   }
-
-  $scope.getData(); //call on page load
+  getData(); //call on page load
 
   $scope.setQueryText = function(event){
     $scope.textAreaVal = event.target.textContent.trim();
   }
 });
-
-// app.controller("workbenchEnhancerController",function($scope){
-//   $scope.groups = [
-//    {
-//      title: 'Dynamic Group Header - 1',
-//      content: 'Dynamic Group Body - 1'
-//    },
-//    {
-//      title: 'Dynamic Group Header - 2',
-//      content: 'Dynamic Group Body - 2'
-//    }
-//  ];
-//
-//  $scope.status = {
-//    isFirstOpen: true,
-//    isFirstDisabled: false
-//  };
-//  $scope.accordionArray = [];
-// })
 
 app.service("dataService",function($q){
   return{
