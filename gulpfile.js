150f071c8fc949135ed3e525448f7c315915f04c
Akhilesh Bhatia <akhilesh.bhatia@siren.io>
2019-12-07 01:29:31 +0000

    created gulp task to copy relevant files to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
 const gulp = require('gulp');
+const filesAndFoldersToCopy = [
+  'fonts/**',
+  'icons/**',
+  'js/**',
+  'scripts/**',
+  'styles/**',
+  'views/**',
+  'background.js',
+  'LICENSE',
+  'manifest.json',
+  'README.md'
+]
 gulp.task('copy', () => {
-  return gulp.src(['./fonts/**']).pipe(gulp.dest('./build'));
+  return gulp.src(filesAndFoldersToCopy, {base: '.'}).pipe(gulp.dest('./build'));
 });
