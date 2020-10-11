3a576e44700d75721c5dfccb005f48871fba0a75
Akhilesh <akhileshbhatia93@gmail.com>
2020-10-11 15:54:28 +0100

    Removes dist folder from commit

--- dist/js/directives/main_extension_dir.js
+++
@@ -1,7 +0,0 @@
-//directive to show the extension div
-app.directive('mainExtension', function () {
-  const mainExtension = {};
-  mainExtension.restrict = 'A';
-  mainExtension.templateUrl = chrome.extension.getURL('views/base.html');
-  return mainExtension;
-});
