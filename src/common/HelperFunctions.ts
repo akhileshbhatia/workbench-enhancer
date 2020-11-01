51a422dff3efb5281ef597a3e437ded3a7fe2cc8
Akhilesh <akhileshbhatia93@gmail.com>
2020-11-01 16:43:47 +0000

    Added pre-commit hook

--- src/common/HelperFunctions.ts
+++ src/common/HelperFunctions.ts
@@ -53,7 +53,7 @@ export function deserializeData(data: string): { output: Map<string, Map<number,
   return { output: finalMap };
 }
 
-export function getFormattedTime(timestamp: string): string {
+export function getFormattedTime(timestamp: string): any {
   const date = new Date(+timestamp * 1000);
   return `${date.getHours()}:${date.getMinutes()}`;
 }
